6b4123936ca494a154393fa7e814596e6fcd3477
sbx_user1051 <sbx_user1051@169.254.133.85>
2019-08-24 20:42:36 +0000

    Sat Aug 24 20:42:36 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    total = 0
+
+    # as there is no example of the input string,
+    # I assume it would be in "3A 2B 1C 3D" format
+
+    # We need an object with prices and special offers
+    # which will be working as our DB.
+    db_values = {
+        'A': {
+            'price': 50,
+            'special_offer': {
+                'qty': 3,
+                'offer'
+            }
+        }
+    }
+
+    return total
+
