de3e5e8a2ac758e6f80d4dd78ea1c90f934cb7f9
sbx_user1051 <sbx_user1051@169.254.121.61>
2019-08-29 20:57:29 +0000

    Thu Aug 29 20:57:29 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -135,6 +135,7 @@ def cart_total():
         # here check for freebies
         if item['eligible_free'] > 0:
             # if there is more freebies than qty
+            print(item['eligible_free'], item['qty'], 'eligible free')
             if item['eligible_free'] > item['qty']:
                 eligible_free = item['eligible_free']
             else:
@@ -181,8 +182,4 @@ print(checkout('CCADDEEBBA')) # 280
 # print(checkout('AAAAAAAAAA')) # 400
 # print(checkout('EE')) # 80
 # print(checkout('EEB')) # 80
-
-
-
-
 
